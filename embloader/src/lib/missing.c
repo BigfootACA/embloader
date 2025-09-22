0195df6977a188bd84e57ef99193b1b45c182cb5
BigfootACA <bigfoot@classfun.cn>
2025-09-23 06:51:05 +0800

    embloader: add efi console control protocol
    Signed-off-by: BigfootACA <bigfoot@classfun.cn>

--- embloader/src/lib/missing.c
+++ embloader/src/lib/missing.c
@@ -1,3 +1,5 @@
 #include "efi-dt-fixup.h"
+#include "efi-console-control.h"
 
 EFI_GUID gEfiDtFixupProtocolGuid = EFI_DT_FIXUP_PROTOCOL_GUID;
+EFI_GUID gEfiConsoleControlProtocolGuid = EFI_CONSOLE_CONTROL_PROTOCOL_GUID;
