2339865ee85de16408ebfe67bfc978057cd72367
BigfootACA <bigfoot@classfun.cn>
2025-09-23 06:58:32 +0800

    embloader: linux: preboot.c: apply devicetree fixup when install fdt
    Signed-off-by: BigfootACA <bigfoot@classfun.cn>

--- embloader/src/linux/preboot.c
+++ embloader/src/linux/preboot.c
@@ -1,3 +1,4 @@
+#include <Library/MemoryAllocationLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 #include <Guid/Fdt.h>
 #include <libfdt.h>
@@ -5,6 +6,75 @@
 #include "embloader.h"
 #include "linuxboot.h"
 #include "efi-utils.h"
+#include "efi-dt-fixup.h"
+
+static EFI_STATUS efi_dtfixup(void *tree) {
+	int ret;
+	UINTN size = 0;
+	EFI_STATUS status;
+	void *buff = NULL;
+	EFI_DT_FIXUP_PROTOCOL *dtfixup = NULL;
+	status = gBS->LocateProtocol(&gEfiDtFixupProtocolGuid, NULL, (VOID**)&dtfixup);
+	if (EFI_ERROR(status) || !dtfixup) {
+		if (status != EFI_NOT_FOUND) log_warning(
+			"failed to locate dtfixup protocol: %s",
+			efi_status_to_string(status)
+		);
+		return status;
+	}
+	if (!(buff = AllocateZeroPool(SIZE_2MB))) {
+		log_error("failed to allocate memory for dtfixup");
+		return EFI_OUT_OF_RESOURCES;
+	}
+	if ((ret = fdt_open_into(tree, buff, SIZE_2MB)) != 0) {
+		log_error("failed to open fdt: %s", fdt_strerror(ret));
+		status = EFI_DEVICE_ERROR;
+		goto done;
+	}
+	fdt_pack(buff);
+	log_debug("applying efi dtfixup protocol");
+	status = dtfixup->Fixup(
+		dtfixup, buff, &size,
+		EFI_DT_APPLY_FIXUPS | EFI_DT_RESERVE_MEMORY
+	);
+	if (status != EFI_BUFFER_TOO_SMALL) {
+		log_warning(
+			"efi dtfixup does not return buffer size: %s",
+			efi_status_to_string(status)
+		);
+		goto done;
+	}
+	if (size > SIZE_2MB) {
+		log_warning("efi dtfixup required too large buffer");
+		status = EFI_BAD_BUFFER_SIZE;
+		goto done;
+	}
+	status = dtfixup->Fixup(
+		dtfixup, buff, &size,
+		EFI_DT_APPLY_FIXUPS | EFI_DT_RESERVE_MEMORY
+	);
+	if (EFI_ERROR(status)) {
+		log_warning(
+			"efi dtfixup failed: %s",
+			efi_status_to_string(status)
+		);
+		goto done;
+	}
+	if ((ret = fdt_check_header(tree)) != 0) {
+		log_error("efi dtfixup produced invalid fdt: %s", fdt_strerror(ret));
+		status = EFI_DEVICE_ERROR;
+		goto done;
+	}
+	if ((ret = fdt_open_into(buff, tree, SIZE_2MB)) != 0) {
+		log_error("efi dtfixup produced invalid fdt: %s", fdt_strerror(ret));
+		status = EFI_DEVICE_ERROR;
+		goto done;
+	}
+	log_info("efi dtfixup applied successfully");
+done:
+	if (buff) FreePool(buff);
+	return status;
+}
 
 /**
  * @brief Install device tree into EFI configuration table
@@ -17,26 +87,22 @@
  */
 EFI_STATUS linux_install_fdt(fdt fdt) {
 	EFI_STATUS status;
-	if (!fdt) return EFI_INVALID_PARAMETER;
+	if (!fdt || fdt_check_header(fdt) < 0) return EFI_INVALID_PARAMETER;
 	void *copied = NULL;
 	int size = fdt_totalsize(fdt);
-	if (size <= 0) return EFI_INVALID_PARAMETER;
-	status = gBS->AllocatePages(
-		AllocateAnyPages,
-		EfiACPIReclaimMemory,
-		EFI_SIZE_TO_PAGES(size),
-		(EFI_PHYSICAL_ADDRESS*) &copied
-	);
-	if (EFI_ERROR(status) || !copied) {
-		log_error(
-			"failed to allocate memory for fdt: %s",
-			efi_status_to_string(status)
-		);
+	if (size <= 0 || size > SIZE_2MB) return EFI_INVALID_PARAMETER;
+	if (!(copied = AllocateAlignedRuntimePages(
+		EFI_SIZE_TO_PAGES(SIZE_2MB), SIZE_2MB
+	))) {
+		log_error("failed to allocate memory for fdt");
 		return EFI_OUT_OF_RESOURCES;
 	}
+	memset(copied, 0, SIZE_2MB);
 	memcpy(copied, fdt, size);
+	if (confignode_path_get_bool(g_embloader.config, "efi.dtfixup", true, NULL))
+		efi_dtfixup(copied);
 	fdt_finish(copied);
-	status = gBS->InstallConfigurationTable(&gFdtTableGuid, fdt);
+	status = gBS->InstallConfigurationTable(&gFdtTableGuid, copied);
 	if (EFI_ERROR(status)) {
 		log_error(
 			"failed to install fdt to system table: %s",
